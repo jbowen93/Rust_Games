e1e5e24f7e63636ec4750c136c08e5b6befbcc24
Josh Bowen <jbowen@apigee.com>
2016-04-09 11:38:29 -0600

    Initial commit of Rust stuff

---
+++ src/phi/mod.rs
@@ -0,0 +1,29 @@
+#[macro_use]
+mod events;
+
+use sdl2::render::Renderer;
+
+struct_events!{
+  keyboard: {
+    key_escape: Escape,
+    key_up: Up,
+    key_down: Down
+  },
+  else: {
+    quit: Quit { .. }
+  }
+}
+
+pub struct Phi<'window> {
+  pub events: Events,
+  pub renderer: Renderer<'window>,
+}
+
+pub enum ViewAction {
+  None,
+  Quit,
+}
+
+pub trait View {
+  fn render(&mut self, context: &mut Phi, elapsed: f64) -> ViewAction;
+}
